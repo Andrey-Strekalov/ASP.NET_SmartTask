9b1f1403d83b22f7ac445613bf8b51b9abfb70ab
Andrey-Strekalov <andreistrekalov1994@gmail.com>
2026-03-26 10:40:22 +0300

    Lab_2: Модели данных, Миграции и Начальное наполнение.

---
+++ SmartTask/SmartTask/Migrations/20260325180459_AddUpdateAtToAllModules.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartTask.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUpdateAtToAllModules : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Tasks",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Tags",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Projects",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_Deadline",
+                table: "Tasks",
+                column: "Deadline");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_Status",
+                table: "Tasks",
+                column: "Status");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_Deadline",
+                table: "Tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_Status",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Projects");
+        }
+    }
+}
